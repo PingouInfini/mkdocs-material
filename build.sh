71319ee27468b4480bb5b163fbd2c41facc7e494
PingouInfini <pingouin@banquise.org>
2023-10-22 02:37:27 +0200

    add docker image push procedure adapting to squidfunk version number

--- build.sh
+++ build.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
 # VARIABLES
-build_dir="context/dockerdist/build"
+build_dir="build"
+
+docker_connect() {
+  # Nom du fichier
+  file="./docker/docker_password.txt"
+
+  # Vérifier si le fichier commence par "#"
+  if [[ $(head -n 1 "$file") == "#"* ]]; then
+    # Demander le mot de passe à l'utilisateur
+    read -s -p "Veuillez entrer votre mot de passe pour Docker Hub: " password
+    echo
+
+    # Utiliser la commande "docker login" avec le mot de passe saisi
+    echo "$password" | docker login --username=pingouinfinihub --password-stdin
+
+  else
+    # Le fichier ne commence pas par "#", utiliser cat pour lire le contenu
+    cat "$file" | docker login --username=pingouinfinihub --password-stdin
+  fi
+}
 
 # Vérifier si le répertoire de destination existe
 if [ -d "$build_dir" ]; then
@@ -14,26 +33,33 @@ git clone https://github.com/squidfunk/mkdocs-material.git "$build_dir"
 
 # Vérifier si la commande git a réussi
 if [ $? -eq 0 ]; then
+    # On recupère la version courante
+    version=$(jq -r '.version' "$build_dir"/package.json)
+
     # On remplace par notre site exemple
-    #rm -rf "$build_dir"/docs
-    cp -r context/dockerdist/mkdocs/mkdocs.yml "$build_dir"/mkdocs.yml
-    cp -r context/dockerdist/mkdocs/.gitignore "$build_dir"/.gitignore
-    cp -r context/dockerdist/mkdocs/content "$build_dir"/content
+    cp -r docker/context/dockerdist/mkdocs/mkdocs.yml "$build_dir"/mkdocs.yml
+    cp -r docker/context/dockerdist/mkdocs/.gitignore "$build_dir"/.gitignore
+    cp -rL docker/context/dockerdist/mkdocs/content "$build_dir"/content
 
     # modification du dockerfile
-    cp -f context/Dockerfile "$build_dir"/Dockerfile
+    cp -f docker/context/Dockerfile "$build_dir"/Dockerfile
 
     # récupération du supervisord
-    cp -r context/dockerdist/supervisord "$build_dir"/supervisord
+    cp -r docker/context/dockerdist/supervisord "$build_dir"/supervisord
 else
     echo "Le clonage du repo GitHub a échoué."
 fi
 
 
 # ... et on lance le build !
-echo "START BUILD..."
+echo "Build de la version v""$version"
 docker build -t pingouinfinihub/mkdocs-material $build_dir
-echo "... DONE !"
+
+docker tag pingouinfinihub/mkdocs-material pingouinfinihub/mkdocs-material:"$version"
+docker tag pingouinfinihub/mkdocs-material pingouinfinihub/mkdocs-material:latest
+docker push pingouinfinihub/mkdocs-material:"$version"
+docker push pingouinfinihub/mkdocs-material:latest
+echo "Done !"
 
 # Pour finir on supprime le clone du repo...
 rm -rf "$build_dir"
